c6a3c5d4e28cac7a7e85a3468ea7b4ee054828b1
Jay Mingle <jkawudi@gmail.com>
2024-02-19 11:55:43 +0000

    Changes

--- src/components/SelectCountry.jsx
+++ src/components/SelectCountry.jsx
@@ -1,11 +1,15 @@
 import React from 'react';
-import {Autocomplete, Grid} from "@mui/material";
+import {Autocomplete, Grid, TextField} from "@mui/material";
 
 const SelectCountry = () => {
     return (
         <div>
-            <Grid item>
-                <Autocomplete xs={{}} value='Option1'  options={['Option 1', 'Option 2', 'Option 3']}/>
+            <Grid item xs={{}}>
+                <Autocomplete
+                    value='Option1'
+                    options={['Option 1', 'Option 2', 'Option 3']}
+                    renderInput{params => <TextField {...params} label="From"}/>}
+                />
             </Grid>
         </div>
     );
