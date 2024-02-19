394cf1a35b181de94bc661ec9dcfbd4a3302e366
Jay Mingle <jkawudi@gmail.com>
2024-02-19 04:16:42 +0000

    Changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
 import { useState } from 'react'
 import './App.css'
+import {Button, Container} from "@mui/material";
 
 function App() {
 
   return (
-    <>
-
-    </>
+    <Container maxWidth="md" sx={{background: "red"}}>
+      <Button variant="contained">Click Me</Button>
+    </Container>
   )
 }
 
