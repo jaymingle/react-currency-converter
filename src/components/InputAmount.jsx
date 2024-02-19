111778d61fd97ab282f4fc0b88a9ec2bd4bee730
Jay Mingle <jkawudi@gmail.com>
2024-02-19 04:29:00 +0000

    Changes

---
+++ src/components/InputAmount.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import {Grid} from '@mui/material'
+
+const InputAmount = () => {
+    return (
+        <div>
+
+        </div>
+    );
+};
+
+export default InputAmount;
