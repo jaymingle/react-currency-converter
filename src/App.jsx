cf63c68add4c917811c9a61e6d36bb760298738f
Jay Mingle <jkawudi@gmail.com>
2024-02-19 13:27:41 +0000

    Changes

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,14 @@ import SwitchCurrency from "./components/SwitchCurrency.jsx";
 function App() {
 
   return (
-    <Container maxWidth="md" sx={{background: "#fdfdfd"}}>
+    <Container maxWidth="md"
+               sx={{
+                   background: "#fdfdfd",
+                   display: "flex",
+                   flexDirection: "column",
+                   gap: "20px",
+                   alignItems: "space-between"
+    }}>
         <Typography variant="h5" sx={{marginTop: "2rem", marginBottom: "2rem"}}> Get Accurate Conversions on the Go </Typography>
         <Grid container spacing={1} sx={{marginTop: "2px", marginLeft: "2px"}}>
             <InputAmount/>
--- src/components/SelectCountry.jsx
+++ src/components/SelectCountry.jsx
@@ -4,12 +4,13 @@ import {Autocomplete, Grid, TextField} from "@mui/material";
 const SelectCountry = () => {
     return (
         <div>
-            <Grid item xs={{}}>
+            <Grid item sx={{maxWidth: "250px", minWidth: "180px"}}>
                 <Autocomplete
                     value='Option1'
                     options={['Option 1', 'Option 2', 'Option 3']}
-                    renderInput{params => <TextField {...params} label="From"}/>}
-                />
+                    renderInput={(params) => (
+                        <TextField {...params} label="From" variant="standard" />
+                    )}                />
             </Grid>
         </div>
     );
