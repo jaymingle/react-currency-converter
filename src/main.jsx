5c92d2c51172c8c0a121221ed310b21fca24649f
Jay Mingle <jkawudi@gmail.com>
2024-02-20 02:35:32 +0000

    Changes

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,7 @@ import '@fontsource/roboto/300.css';
 import '@fontsource/roboto/400.css';
 import '@fontsource/roboto/500.css';
 import '@fontsource/roboto/700.css';
-import CurrencyProvider from "./context/CurrencyContext.jsx";
+import CurrencyProvider from "./context/CurrencyContext";
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
