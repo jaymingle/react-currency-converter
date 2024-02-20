ce07c0775e256887bd72bbbf916106eb26cd041a
Jay Mingle <jkawudi@gmail.com>
2024-02-20 00:48:48 +0000

    Changes

--- src/components/SelectCountry.jsx
+++ src/components/SelectCountry.jsx
@@ -17,7 +17,7 @@ const SelectCountry = () => {
                     disablePortal
                     id="combo-box-demo"
                     value='Option1'
-                    options={['Option 1', 'Option 2', 'Option 3']}
+                    options={dataCountry}
                     renderInput={(params) => (
                         <TextField {...params} label="From"/>
                     )} />
