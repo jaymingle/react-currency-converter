916ed1783961fdf14eb21879014c53fcc759c538
Jay Mingle <jkawudi@gmail.com>
2024-02-20 02:50:45 +0000

    Changes

--- src/components/SwitchCurrency.jsx
+++ src/components/SwitchCurrency.jsx
@@ -1,14 +1,27 @@
-import React from 'react';
+import React, {useContext} from 'react';
 import {Button, Grid} from "@mui/material";
 import CompareArrowsIcon from '@mui/icons-material/CompareArrows';
+import {CurrencyContext} from "../context/CurrencyContext.jsx";
 
 const SwitchCurrency = () => {
+
+    const {
+        fromCurrency,
+        setFromCurrency,
+        toCurrency,
+        setToCurrency
+    } = useContext(CurrencyContext)
+
+    const switchCurrencyHandler = () => {
+
+    }
+
     return (
             <Grid item xs={12} md={"auto"}>
                 <Button sx={{
                     borderRadius: 1,
                     height: '100%',
-                }}>
+                }} onClick={switchCurrencyHandler}>
                     <CompareArrowsIcon sx={{
                         fontSize: 30
                     }}/>
