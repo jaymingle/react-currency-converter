05939d743e8cbde26bd88d2877de9d14c39352f4
Jay Mingle <jkawudi@gmail.com>
2024-02-20 00:50:59 +0000

    Changes

--- src/components/SelectCountry.jsx
+++ src/components/SelectCountry.jsx
@@ -1,12 +1,20 @@
 import React from 'react';
-import {Autocomplete, Grid, TextField} from "@mui/material";
+import {Autocomplete, Grid, Skeleton, TextField} from "@mui/material";
 import useAxios from "../hooks/useAxios";
 
 const SelectCountry = () => {
 
-    const [data] = useAxios('https://restcountries.com/v3.1/all')
-    console.log(data)
+    const [data, loaded, error] = useAxios('https://restcountries.com/v3.1/all')
 
+    if(loaded){
+        return (
+            <Grid item xs={12} md={3}>
+                <Skeleton variant={"rounded"} height={60}/>
+            </Grid>
+            )
+    }
+
+    console.log(data)
     const dataFilter = data.filter(item => "currencies" in item)
     console.log(dataFilter)
     const dataCountry = dataFilter.map(country => `${country.flag} ${Object.keys(country.currencies)[0]} - ${country.name.common}`)
