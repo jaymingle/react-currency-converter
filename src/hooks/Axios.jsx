436d4978d079216127d1172e7a96b231685de4d4
Jay Mingle <jkawudi@gmail.com>
2024-02-20 00:24:54 +0000

    Changes

---
+++ src/hooks/Axios.jsx
@@ -0,0 +1,20 @@
+import React, {useState, useEffect} from 'react';
+
+const Axios = () => {
+
+    const [data, setData] = useState([])
+    const [error, setError] = useState(null)
+    const [loaded, setLoaded] = useState(false)
+
+    useEffect(() => {
+
+    }, [])
+
+    return (
+        <div>
+
+        </div>
+    );
+};
+
+export default Axios;
