4cee6618613b34ae5ee18da037cc67bfabc1a027
Jay Mingle <jkawudi@gmail.com>
2024-02-20 00:21:42 +0000

    Changes

--- src/App.jsx
+++ src/App.jsx
@@ -7,13 +7,22 @@ import SwitchCurrency from "./components/SwitchCurrency.jsx";
 
 function App() {
 
+    const BoxStyles ={
+        background: "#fdfdfd",
+        textAlign: "center",
+        color: "#222",
+        minHeight: "20rem",
+        borderRadius: 2,
+        padding: "4rem 2rem",
+        boxShadow: "0px 10px 15px -3px rgba(0,0,0,0.1)",
+        position: "relative",
+        marginTop: "10rem"
+    }
+
   return (
-    <Container maxWidth="md"
-               sx={{
-                   background: "#fdfdfd",
-    }}>
+    <Container maxWidth="md" sx={BoxStyles}>
         <Typography variant="h5" sx={{marginTop: "2rem", marginBottom: "2rem"}}> Get Accurate Conversions on the Go </Typography>
-        <Grid container spacing={2} sx={{marginTop: "2px", marginLeft: "2px"}}>
+        <Grid container spacing={2}>
             <InputAmount/>
             <SelectCountry/>
             <SwitchCurrency/>
